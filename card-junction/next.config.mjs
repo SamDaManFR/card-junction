55d5228628d0266273cff2a4a0741113dd5ce65f
SamDaManFR <sewalker0910@gmail.com>
2026-02-14 00:46:33 -0600

    Update next.config.mjs

--- card-junction/next.config.mjs
+++ card-junction/next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'i.ebayimg.com' },
-      { protocol: 'https', hostname: 'thumbs.ebaystatic.com' },
+      { protocol: "https", hostname: "i.ebayimg.com" },
+      { protocol: "https", hostname: "thumbs.ebaystatic.com" },
     ],
   },
 };
