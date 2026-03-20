9911f943782aef16fedcf89fabaeaa085169828b
SamDaManFR <sewalker0910@gmail.com>
2026-03-19 19:08:55 -0500

    Update page.tsx

--- card-junction/app/grading/psa/page.tsx
+++ card-junction/app/grading/psa/page.tsx
@@ -539,7 +539,7 @@ export default function Page() {
                             {it.title}
                           </p>
 
-                          {showDealBadge && discountPct >= 10 && price != null && (
+                          {showDealBadge && discountPct >= 1 && price != null && (
                             <div
                               style={{
                                 display: "inline-block",
